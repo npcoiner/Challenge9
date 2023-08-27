8ee125e46e75d3658c8a08f784f096d67b04649c
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-26 20:44:18 -0700

    Added messages Added writeToFile code Added init() code

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,59 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown')
+const inquirer = require('inquirer');
+const {writeFile} = require('fs').promises;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "Enter Title:",
+        name: "Title"
+    },
+    {
+        type: 'input',
+        message: "Enter Description:",
+        name: "Description"
+    },
+    {
+        type: 'input',
+        message: "Enter Title:",
+        name: "Title"
+    },
+    {
+        type: 'input',
+        message: "Enter Title:",
+        name: "Title"
+    },
+    {
+        type: 'input',
+        message: "Enter Title:",
+        name: "Title"
+    },
+    {
+        type: 'input',
+        message: "Enter Title:",
+        name: "Title"
+    },
+    {
+        type: 'input',
+        message: "Enter Title:",
+        name: "Title"
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    const md = generateMarkdown(data);
+    writeFile(fileName,md);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => {writeToFile(`newREADME.md`,answers)})
+    .then(() => console.log('done'))
 
+}   
 // Function call to initialize app
 init();
