88edf345c3b7200059147138c610880739163733
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-26 21:19:37 -0700

    Added full questions and validations

--- Develop/index.js
+++ Develop/index.js
@@ -8,38 +8,96 @@ const questions = [
     {
         type: 'input',
         message: "Enter Title:",
-        name: "Title"
+        name: "title",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
     {
         type: 'input',
         message: "Enter Description:",
-        name: "Description"
+        name: "description",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
     {
         type: 'input',
-        message: "Enter Title:",
-        name: "Title"
+        message: "Enter Installation Instructions:",
+        name: "installation",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
     {
         type: 'input',
-        message: "Enter Title:",
-        name: "Title"
+        message: "Enter Usage Information:",
+        name: "usage",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
     {
         type: 'input',
-        message: "Enter Title:",
-        name: "Title"
+        message: "Enter Contribution Information:",
+        name: "contribute",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
     {
         type: 'input',
-        message: "Enter Title:",
-        name: "Title"
+        message: "Enter Tests Information:",
+        name: "tests",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
+    },
+    {
+        type: 'list',
+        message: "Choose a license:",
+        name: "license",
+        choices:[
+            "apache-2.0",
+            "bsl-1.0",
+            "bsd-2-clause",
+            "bsd-3-clause",
+            "cc0-1.0",
+            "cc-by-4.0",
+            "wtfpl",
+            "epl-1.0",
+            "eupl-1.1",
+            "agpl-3.0",
+            "gpl-2.0",
+            "gpl-3.0",
+            "lgpl-3.0",
+            "isc",
+            "mit",
+            "mpl-2.0",
+            "unlicense",
+            "zlib",
+        ],
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
     {
         type: 'input',
-        message: "Enter Title:",
-        name: "Title"
+        message: "Enter Github Username:",
+        name: "github",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
     },
+    {
+        type: 'input',
+        message: "Enter Email:",
+        name: "email",
+        validate: (input) => {
+            return input ? true : console.log(message), false
+        }
+    },
+
 ];
 
 // TODO: Create a function to write README file
