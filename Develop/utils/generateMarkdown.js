7d30a23ccdc82cf0d49dd81c2d2515b855c3e19a
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-08-26 20:54:15 -0700

    Finished generateMarkdown text. Now to add functionality

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,9 +14,56 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  
   return `# ${data.title}
+  ## License Badge
+  ${renderLicenseBadge(data.license)}
 
-`;
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+
+  ## Installation
+
+  ${data.installation}
+  ## Usage
+
+  ${data.usage}
+  ## Credits
+
+  ${data.credits}
+
+  ## License
+
+  ${renderLicenseSection(data.license)}
+  ${renderLicenseLink(license)}
+  ---
+
+
+  ## Features
+
+  ${data.features}
+
+  ## How to Contribute
+
+  ${data.contribute}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Questions?
+  
+  github.com/${data.github}
+  email :${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
